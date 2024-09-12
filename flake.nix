4d1389172510fb6cec9311bab802a0ca1c218e34
Artem Trokhymchuk <trkhmchk@gmail.com>
2024-09-12 23:35:12 +0300

    add Lesson2

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "node env";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              nodejs
+            ];
+            shellHook = ''
+            '';
+          };
+        }
+      );
+}
+
